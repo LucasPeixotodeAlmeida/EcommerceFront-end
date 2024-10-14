98af5adf20c6a63e362bafcb220c7976922bb6f5
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-10-14 19:51:58 -0300

    Definindo as rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,26 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import{Routes, RouterModule} from '@angular/router'
+const routes: Routes = [
+  //a ordem das rotas influencia: a mais especifica sempre fica no topo
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+
+  //o FULL é utilizado pra garantir que o match seja 100%
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+
+  //"**" significa que só cai nessa rota se nao for compativel com  nenhuma rota acima
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
